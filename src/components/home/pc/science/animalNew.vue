299a974117b3aef70704079aed6de7f445dc1e76
hyUrbanManager <936947038@qq.com>
2019-02-26 23:57:37 +0800

    [feature]完成动物新闻页面和数据装填。

--- src/components/home/pc/science/animalNew.vue
+++ src/components/home/pc/science/animalNew.vue
@@ -4,7 +4,30 @@
     <!-- 头部 -->
     <header-vue></header-vue>
     <!-- 动物新闻 -->
-    <div class="new-img"></div>
+    <div class="containMain">
+      <div class="innerPageBg">
+        <img src="https://www.gzzoo.com/UploadFile/7.jpg">
+      </div>
+      <!-- 新闻 -->
+      <div class="innerPageCon_1">
+        <div class="panel-group" id="accordion">
+          <div class="panel panel-default" v-for="(n, index) in news" :key="index">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a
+                  data-toggle="collapse"
+                  data-parent="#accordion"
+                  v-bind:href="'#collapse' + index"
+                >{{ n.title }}</a>{{ n.time }}
+              </h4>
+            </div>
+            <div v-bind:id="'collapse' + index" class="panel-collapse collapse">
+              <div class="panel-body">{{ n.content }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- 尾部 -->
     <footer-vue></footer-vue>
   </div>
@@ -13,16 +36,49 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      news: {}
+    };
+  },
+  created() {
+    this.$ajax
+      .get("animalNews.json")
+      .then(res => {
+        this.news = res.data.animalNews;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 };
 </script>
 <style scoped>
-.new-img {
-  margin-top: 40px;
+.containMain {
+  position: relative;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  overflow-x: hidden;
+}
+.innerPageBg {
+  position: absolute;
+  left: 0;
+  top: 0;
   width: 100%;
-  height: 400px;
-  background-image: url("https://www.gzzoo.com/UploadFile/7.jpg");
-  background-size: cover;
+  z-index: -10;
+}
+.innerPageBg img {
+  width: 100%;
+  height: auto;
+}
+.innerPageCon_1 {
+  width: 1200px;
+  margin-top: 300px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: azure;
+  -webkit-animation: appear-up 1s ease;
+  -moz-animation: appear-up 1s ease;
+  -o-animation: appear-up 1s ease;
+  animation: appear-up 1s ease;
 }
 </style>
